3a20a060ddaf375309c73e20ad10ab485d4bee00
Carlos Cabrera <cacabrer@adobe.com>
2019-04-11 16:56:20 -0500

    removed addoninfo

--- .storybook/config.js
+++ .storybook/config.js
@@ -3,14 +3,9 @@ import { themes } from '@storybook/theming'
 
 import { withKnobs } from '@storybook/addon-knobs'
 import { withA11y } from '@storybook/addon-a11y'
-import { withInfo } from '@storybook/addon-info'
 
 addDecorator(withKnobs)
 addDecorator(withA11y)
-addDecorator(withInfo({
-    header: false,
-    inline: true,
-}))
 
 addParameters({
     options: {
