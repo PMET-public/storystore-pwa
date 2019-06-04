e79c2c982716e63a6dd0abc4f1a8ecb11e246ea8
Carlos Cabrera <cacabrer@adobe.com>
2019-06-03 19:34:39 -0500

    poc

--- containers/App.tsx
+++ containers/App.tsx
@@ -1,10 +1,10 @@
 import React, { FunctionComponent, Fragment } from 'react'
 import { gql } from 'apollo-boost'
 import { Query } from 'react-apollo'
-import { getFullPageTitle } from 'luma-storybook/dist/lib/helpers'
-import _App from 'luma-storybook/dist/components/App'
+import { getFullPageTitle } from '@app/lib/helpers'
 import { DocumentMetadataProps } from '@app/components/DocumentMetadata'
 import DocumentMetadata from '@app/components/DocumentMetadata'
+import { HotSpot, HotSpotContainer } from '../../luma-storybook/dist/components/HotSpots'
 
 const APP_SHELL_QUERY = gql`
     query AppShellQuery {
@@ -25,7 +25,7 @@ const APP_SHELL_QUERY = gql`
 const App: FunctionComponent<any> = ({ children, ...rest }) => (
     <Query query={APP_SHELL_QUERY} fetchPolicy="cache-first" errorPolicy="all">
         {({ loading, data: {
-            flashMessage,
+            // flashMessage,
             storeConfig: {
                 title_prefix,
                 title_suffix,
@@ -44,9 +44,35 @@ const App: FunctionComponent<any> = ({ children, ...rest }) => (
             return (
                 <Fragment>
                     <DocumentMetadata {...metadata} />
-                    <_App flashMessage={flashMessage} {...rest}>
-                        {children}
-                    </_App>
+
+                    <HotSpotContainer
+                        image="https://pmet-public.github.io/luma-storybook/static/media/products-hotspots.b3dba30c.jpg"
+                        description="A lot of stuffs"
+                    >
+                        <HotSpot
+                            coords={{ x: 15, y: 42 }}
+                            id={0}
+                            label="Sweater"
+                        >
+                            <div>Knit Sweater - $29.99</div>
+                        </HotSpot>
+                        <HotSpot
+                            coords={{ x: 78, y: 30 }}
+                            id={1}
+                            label="Bag"
+                        >
+                            <div>Handbag - $19.99</div>
+                        </HotSpot>
+                        <HotSpot
+                            coords={{ x: 63, y: 75 }}
+                            id={2}
+                            label="Pants"
+                        >
+                            <div>Cotton Chinos - $29.99</div>
+                        </HotSpot>
+                    </HotSpotContainer>
+
+                    {children}
                 </Fragment>
             )
         }}
