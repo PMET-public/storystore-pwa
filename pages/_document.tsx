50e49931bb636a5da0675296eb37b2aa4b9ce964
Carlos A. Cabrera <cacabrer@adobe.com>
2019-11-26 15:47:37 -0600

    test with credentials

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -39,13 +39,20 @@ export default class extends Document {
                     <meta name="theme-color" content="#222222" />
                     <link rel="shortcut icon" href="/static/favicon.ico" />
 
+                    {/* iOS */}
+                    {/* <meta name="apple-mobile-web-app-capable" content="yes" />
+                    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+                    <meta name="apple-mobile-web-app-title" content="Luma" />
+                    <link rel="apple-touch-startup-image" />
+                    <link rel="apple-touch-icon" href="/static/ios-icon.png" /> */}
+
                     {/* Web App Manifest  */}
                     <link rel="manifest" href="/manifest.webmanifest" crossOrigin="use-credentials" />
                     <script
                         async
                         src="https://cdn.jsdelivr.net/npm/pwacompat@2.0.10/pwacompat.min.js"
                         integrity="sha384-I1iiXcTSM6j2xczpDckV+qhhbqiip6FyD6R5CpuqNaWXvyDUvXN5ZhIiyLQ7uuTh"
-                        crossOrigin="anonymous"
+                        crossOrigin="use-credentials"
                     />
 
                     {/* Fonts */}
