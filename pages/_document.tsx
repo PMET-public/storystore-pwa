085a9206226a7ef76dcb085f1b20d457de4b527b
Carlos A. Cabrera <cacabrer@adobe.com>
2019-11-26 13:49:03 -0600

    default itouch icon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -40,9 +40,11 @@ export default class extends Document {
                     <meta name=" theme-color" content="#222222" />
 
                     {/* iOS */}
-                    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
-
                     <meta name="apple-mobile-web-app-capable" content="yes" />
+                    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+                    <meta name="apple-mobile-web-app-title" content="Luma">
+
+                    <link rel="apple-touch-icon" href="/static/icons/icon_180x180.png" />
                     <link rel="apple-touch-icon" href="/static/icons/icon_512x512.png" sizes="512x512" />
                     <link rel="apple-touch-icon" href="/static/icons/icon_384x384.png" sizes="384x384" />
                     <link rel="apple-touch-icon" href="/static/icons/icon_256x256.png" sizes="256x256" />
