17e22b8bb3cda421297f6350c4d09f66bb4086f4
Carlos A. Cabrera <cacabrer@adobe.com>
2019-11-26 16:26:03 -0600

    Using datauri for images

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,7 @@
 import Document, { Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
+import path from 'path'
+import { sync as dataUri } from 'datauri'
 
 export default class extends Document {
     static async getInitialProps(ctx: any) {
@@ -40,20 +42,17 @@ export default class extends Document {
                     <link rel="shortcut icon" href="/static/favicon.ico" />
 
                     {/* iOS */}
-                    {/* <meta name="apple-mobile-web-app-capable" content="yes" />
+                    <meta name="apple-mobile-web-app-capable" content="yes" />
                     <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
                     <meta name="apple-mobile-web-app-title" content="Luma" />
                     <link rel="apple-touch-startup-image" />
-                    <link rel="apple-touch-icon" href="/static/ios-icon.png" /> */}
+                    <link
+                        rel="apple-touch-icon"
+                        href={dataUri(path.relative(process.cwd(), './public/static/ios-icon.png'))}
+                    />
 
                     {/* Web App Manifest  */}
                     <link rel="manifest" href="/manifest.webmanifest" crossOrigin="use-credentials" />
-                    <script
-                        async
-                        src="https://cdn.jsdelivr.net/npm/pwacompat@2.0.10/pwacompat.min.js"
-                        integrity="sha384-I1iiXcTSM6j2xczpDckV+qhhbqiip6FyD6R5CpuqNaWXvyDUvXN5ZhIiyLQ7uuTh"
-                        crossOrigin="use-credentials"
-                    />
 
                     {/* Fonts */}
                     <link rel="stylesheet" href="/static/fonts.css" />
