c5d61eb3ccfd9e7c6a815185442906a06207a850
Carlos A. Cabrera <cacabrer@adobe.com>
2019-12-03 13:24:10 -0600

    moved assets

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -50,44 +50,43 @@ export default class extends Document {
 
                     <link
                         rel="apple-touch-startup-image"
-                        href="/static/splash/640x1136.png"
+                        href="https://raw.githubusercontent.com/PMET-public/luma-ui/develop/public/splash/640x1136.png"
                         media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
                     />
                     <link
                         rel="apple-touch-startup-image"
-                        href="/static/splash/750x1294.png"
+                        href="https://raw.githubusercontent.com/PMET-public/luma-ui/develop/public/splash/750x1294.png"
                         media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
                     />
                     <link
                         rel="apple-touch-startup-image"
-                        href="/static/splash/1242x2148.png"
+                        href="https://raw.githubusercontent.com/PMET-public/luma-ui/develop/public/splash/1242x2148.png"
                         media="(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
                     />
                     <link
                         rel="apple-touch-startup-image"
-                        href="/static/splash/1125x2436.png"
+                        href="https://raw.githubusercontent.com/PMET-public/luma-ui/develop/public/splash/1125x2436.png"
                         media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
                     />
                     <link
                         rel="apple-touch-startup-image"
-                        href="/static/splash/1536x2048.png"
+                        href="https://raw.githubusercontent.com/PMET-public/luma-ui/develop/public/splash/1536x2048.png"
                         media="(min-device-width: 768px) and (max-device-width: 1024px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)"
                     />
                     <link
                         rel="apple-touch-startup-image"
-                        href="/static/splash/1668x2224.png"
+                        href="https://raw.githubusercontent.com/PMET-public/luma-ui/develop/public/splash/1668x2224.png"
                         media="(min-device-width: 834px) and (max-device-width: 834px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)"
                     />
                     <link
                         rel="apple-touch-startup-image"
-                        href="/static/splash/2048x2732.png"
+                        href="https://raw.githubusercontent.com/PMET-public/luma-ui/develop/public/splash/2048x2732.png"
                         media="(min-device-width: 1024px) and (max-device-width: 1024px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)"
                     />
 
                     <link
                         rel="apple-touch-icon"
-                        href="/static/icons/apple-touch-icon.png"
-                        crossOrigin="use-credentials"
+                        href="https://raw.githubusercontent.com/PMET-public/luma-ui/develop/public/icons/apple-touch-icon.png"
                     />
 
                     {/* Web App Manifest  */}
