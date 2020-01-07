2e11e2ddd6ca1df0668d498a8b1fa87e453cdc8e
Carlos A. Cabrera <cacabrer@adobe.com>
2020-01-07 17:59:42 -0600

    fixed url

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -61,102 +61,102 @@ export default class extends Document {
                     <link
                         rel="apple-touch-startup-image"
                         media="screen and (device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
-                        href={`/api/image?url=https://luma-pwa.now.sh/static/splash/master.png&width=1136&height=640&format=png`}
+                        href={`/api/images?url=https://luma-pwa.now.sh/static/splash/master.png&width=1136&height=640&format=png`}
                     />
                     <link
                         rel="apple-touch-startup-image"
                         media="screen and (device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)"
-                        href={`/api/image?url=https://luma-pwa.now.sh/static/splash/master.png&width=2436&height=1125&format=png`}
+                        href={`/api/images?url=https://luma-pwa.now.sh/static/splash/master.png&width=2436&height=1125&format=png`}
                     />
                     <link
                         rel="apple-touch-startup-image"
                         media="screen and (device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
-                        href={`/api/image?url=https://luma-pwa.now.sh/static/splash/master.png&width=1792&height=828&format=png`}
+                        href={`/api/images?url=https://luma-pwa.now.sh/static/splash/master.png&width=1792&height=828&format=png`}
                     />
                     <link
                         rel="apple-touch-startup-image"
                         media="screen and (device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
-                        href={`/api/image?url=https://luma-pwa.now.sh/static/splash/master.png&width=828x&height=792&format=png`}
+                        href={`/api/images?url=https://luma-pwa.now.sh/static/splash/master.png&width=828x&height=792&format=png`}
                     />
                     <link
                         rel="apple-touch-startup-image"
                         media="screen and (device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
-                        href={`/api/image?url=https://luma-pwa.now.sh/static/splash/master.png&width=1334&height=750&format=png`}
+                        href={`/api/images?url=https://luma-pwa.now.sh/static/splash/master.png&width=1334&height=750&format=png`}
                     />
                     <link
                         rel="apple-touch-startup-image"
                         media="screen and (device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
-                        href={`/api/image?url=https://luma-pwa.now.sh/static/splash/master.png&width=1242&height=2688&format=png`}
+                        href={`/api/images?url=https://luma-pwa.now.sh/static/splash/master.png&width=1242&height=2688&format=png`}
                     />
                     <link
                         rel="apple-touch-startup-image"
                         media="screen and (device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)"
-                        href={`/api/image?url=https://luma-pwa.now.sh/static/splash/master.png&width=2208&height=1242&format=png`}
+                        href={`/api/images?url=https://luma-pwa.now.sh/static/splash/master.png&width=2208&height=1242&format=png`}
                     />
                     <link
                         rel="apple-touch-startup-image"
                         media="screen and (device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
-                        href={`/api/image?url=https://luma-pwa.now.sh/static/splash/master.png&width=1125&height=2436&format=png`}
+                        href={`/api/images?url=https://luma-pwa.now.sh/static/splash/master.png&width=1125&height=2436&format=png`}
                     />
                     <link
                         rel="apple-touch-startup-image"
                         media="screen and (device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
-                        href={`/api/image?url=https://luma-pwa.now.sh/static/splash/master.png&width=1242&height=2208&format=png`}
+                        href={`/api/images?url=https://luma-pwa.now.sh/static/splash/master.png&width=1242&height=2208&format=png`}
                     />
                     <link
                         rel="apple-touch-startup-image"
                         media="screen and (device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
-                        href={`/api/image?url=https://luma-pwa.now.sh/static/splash/master.png&width=2732&height=2048&format=png`}
+                        href={`/api/images?url=https://luma-pwa.now.sh/static/splash/master.png&width=2732&height=2048&format=png`}
                     />
                     <link
                         rel="apple-touch-startup-image"
                         media="screen and (device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)"
-                        href={`/api/image?url=https://luma-pwa.now.sh/static/splash/master.png&width=2688&height=1242&format=png`}
+                        href={`/api/images?url=https://luma-pwa.now.sh/static/splash/master.png&width=2688&height=1242&format=png`}
                     />
                     <link
                         rel="apple-touch-startup-image"
                         media="screen and (device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
-                        href={`/api/image?url=https://luma-pwa.now.sh/static/splash/master.png&width=2224&height=1668&format=png`}
+                        href={`/api/images?url=https://luma-pwa.now.sh/static/splash/master.png&width=2224&height=1668&format=png`}
                     />
                     <link
                         rel="apple-touch-startup-image"
                         media="screen and (device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
-                        href={`/api/image?url=https://luma-pwa.now.sh/static/splash/master.png&width=750x&height=334&format=png`}
+                        href={`/api/images?url=https://luma-pwa.now.sh/static/splash/master.png&width=750x&height=334&format=png`}
                     />
                     <link
                         rel="apple-touch-startup-image"
                         media="screen and (device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
-                        href={`/api/image?url=https://luma-pwa.now.sh/static/splash/master.png&width=2048&height=2732&format=png`}
+                        href={`/api/images?url=https://luma-pwa.now.sh/static/splash/master.png&width=2048&height=2732&format=png`}
                     />
                     <link
                         rel="apple-touch-startup-image"
                         media="screen and (device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
-                        href={`/api/image?url=https://luma-pwa.now.sh/static/splash/master.png&width=2388&height=1668&format=png`}
+                        href={`/api/images?url=https://luma-pwa.now.sh/static/splash/master.png&width=2388&height=1668&format=png`}
                     />
                     <link
                         rel="apple-touch-startup-image"
                         media="screen and (device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
-                        href={`/api/image?url=https://luma-pwa.now.sh/static/splash/master.png&width=1668&height=2224&format=png`}
+                        href={`/api/images?url=https://luma-pwa.now.sh/static/splash/master.png&width=1668&height=2224&format=png`}
                     />
                     <link
                         rel="apple-touch-startup-image"
                         media="screen and (device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
-                        href={`/api/image?url=https://luma-pwa.now.sh/static/splash/master.png&width=640x&height=136&format=png`}
+                        href={`/api/images?url=https://luma-pwa.now.sh/static/splash/master.png&width=640x&height=136&format=png`}
                     />
                     <link
                         rel="apple-touch-startup-image"
                         media="screen and (device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
-                        href={`/api/image?url=https://luma-pwa.now.sh/static/splash/master.png&width=1668&height=2388&format=png`}
+                        href={`/api/images?url=https://luma-pwa.now.sh/static/splash/master.png&width=1668&height=2388&format=png`}
                     />
                     <link
                         rel="apple-touch-startup-image"
                         media="screen and (device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
-                        href={`/api/image?url=https://luma-pwa.now.sh/static/splash/master.png&width=2048&height=1536&format=png`}
+                        href={`/api/images?url=https://luma-pwa.now.sh/static/splash/master.png&width=2048&height=1536&format=png`}
                     />
                     <link
                         rel="apple-touch-startup-image"
                         media="screen and (device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
-                        href={`/api/image?url=https://luma-pwa.now.sh/static/splash/master.png&width=1536&height=2048&format=png`}
+                        href={`/api/images?url=https://luma-pwa.now.sh/static/splash/master.png&width=1536&height=2048&format=png`}
                     />
                 </Head>
 
