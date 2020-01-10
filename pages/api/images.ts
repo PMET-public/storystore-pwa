f258cd7343a4cb059afd820b63cb11c817ef6518
Carlos A. Cabrera <cacabrer@adobe.com>
2020-01-10 15:57:13 -0600

    removed quality

--- pages/api/images.ts
+++ pages/api/images.ts
@@ -8,11 +8,9 @@ const { MAGENTO_URL = '' } = process.env
 export const ImagesApi = (req: NextApiRequest, res: NextApiResponse) => {
     const url = new URL(req.query.url.toString(), MAGENTO_URL)
     const format = req.query.format?.toString() || 'jpeg'
-    const quality = req.query.quality ? Number(req.query.quality) : 100
 
     const width = req.query.width && Number(req.query.width)
     const height = req.query.height && Number(req.query.height)
-    const fit = (req.query.fit as '') || 'cover'
 
     res.setHeader('Content-Type', `image/${format}`)
 
@@ -32,16 +30,14 @@ export const ImagesApi = (req: NextApiRequest, res: NextApiResponse) => {
         const image = sharp(body)
 
         if (width) {
-            image.resize(width > 2000 ? 2000 : width, height ? (height > 2000 ? 2000 : height) : undefined, {
-                fit,
-            })
+            image.resize(width > 2000 ? 2000 : width, height ? (height > 2000 ? 2000 : height) : undefined)
         }
 
         image
-            .toFormat(format, { quality })
+            .toFormat(format)
             .toBuffer()
             .then(responseBody => {
-                res.setHeader('Cache-Control', 'max-age=0, s-maxage=2592000')
+                res.setHeader('Cache-Control', 'max-age=2592000, immutable')
                 res.send(responseBody)
             })
             .catch(error => {
