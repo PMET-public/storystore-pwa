0d9080c61dce76b00e2ef01dad3526a5c8622fe2
Carlos A. Cabrera <cacabrer@adobe.com>
2020-02-13 12:35:20 -0600

    fixed api functions

--- pages/api/images.ts
+++ pages/api/images.ts
@@ -5,25 +5,30 @@ import { NextApiRequest, NextApiResponse } from 'next'
 const maxAge = 30 * 86400 // 30 days
 
 export const ImagesApi = async (req: NextApiRequest, res: NextApiResponse) => {
-    return new Promise((resolve, reject) => {
-        const url = req.query.url.toString()
+    return new Promise(resolve => {
+        try {
+            const url = req.query.url.toString()
 
-        req.pipe(
-            request.get({
-                qs: req.query,
-                url: new URL(url, process.env.MAGENTO_URL).href,
-                pool: {
-                    maxSockets: Infinity,
-                },
-            })
-        )
-            .on('response', res => {
-                /** Use Edge Case in now.sh */
-                res.headers['Cache-Control'] = `max-age=${maxAge}, immutable`
-            })
-            .pipe(res)
-            .on('error', reject)
-            .on('response', resolve)
+            req.pipe(
+                request.get({
+                    qs: req.query,
+                    url: new URL(url, process.env.MAGENTO_URL).href,
+                    pool: {
+                        maxSockets: Infinity,
+                    },
+                })
+            )
+                .once('response', res => {
+                    /** Use Edge Case in now.sh */
+                    res.headers['Cache-Control'] = `max-age=${maxAge}, immutable`
+                })
+                .pipe(res)
+                .once('finish', () => resolve())
+        } catch (error) {
+            console.error(error)
+            res.status(500).end()
+            return resolve()
+        }
     })
 }
 
