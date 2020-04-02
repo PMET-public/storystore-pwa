314df1c16e9e931601db286a49af9205e90bb6c8
Carlos A. Cabrera <cacabrer@adobe.com>
2020-04-01 20:14:59 -0500

    test

--- lib/workbox.ts
+++ lib/workbox.ts
@@ -1,6 +1,6 @@
-import { registerRoute, setCatchHandler } from 'workbox-routing'
+import { registerRoute, setCatchHandler, setDefaultHandler } from 'workbox-routing'
 import { precacheAndRoute, cleanupOutdatedCaches, matchPrecache } from 'workbox-precaching'
-import { CacheFirst, StaleWhileRevalidate } from 'workbox-strategies'
+import { CacheFirst, StaleWhileRevalidate, NetworkFirst } from 'workbox-strategies'
 import { ExpirationPlugin } from 'workbox-expiration'
 import { CacheableResponsePlugin } from 'workbox-cacheable-response'
 import { skipWaiting, clientsClaim, WorkboxPlugin, RouteMatchCallback } from 'workbox-core'
@@ -34,30 +34,6 @@ const getRoutePaths = (paths: string[]) => {
 /**
  * Routes
  */
-
-//  Pages
-const matchPages: RouteMatchCallback = ({ url, event }) => {
-    const { request } = event
-    return url.pathname !== '/basic-auth' && request.method === 'GET' && request.destination === 'document'
-}
-
-registerRoute(
-    matchPages,
-    new StaleWhileRevalidate({
-        cacheName: 'pages',
-        fetchOptions,
-        plugins,
-    })
-)
-
-// registerRoute(
-//     getRoutePaths(['/search', '/cart', '/checkout', '/offline']), // other pages
-//     new StaleWhileRevalidate({
-//         cacheName: 'pages',
-//         fetchOptions,
-//         plugins,
-//     })
-// )
 
 // Images API
 registerRoute(
@@ -101,29 +77,28 @@ registerRoute(
  * Fallback (default handler)
  */
 
-// setDefaultHandler(args => {
-//     const { request } = args.event
+setDefaultHandler(args => {
+    const { url, event } = args
 
-//     console.group()
-//     console.log(args)
-//     if (request.method === 'GET' && request.destination === 'document') {
-//         console.log('inside')
+    if (
+        url?.pathname !== '/basic-auth' &&
+        event?.request.method === 'GET' &&
+        event?.request.destination === 'document'
+    ) {
+        return new NetworkFirst({
+            cacheName: 'pages',
+            fetchOptions,
+            plugins,
+        }).handle(args)
+    }
 
-//         return new StaleWhileRevalidate({
-//             cacheName: 'default',
-//             fetchOptions,
-//             plugins,
-//         }).handle(args)
-//     }
-
-//     console.log('outside')
-
-//     console.groupEnd()
-//     return fetch(request, fetchOptions)
-// })
+    console.log(event?.request, fetchOptions)
+    return fetch(event?.request, fetchOptions)
+})
 
 setCatchHandler(({ event }) => {
-    if (event?.request.method === 'GET' && event?.request.destination === 'document') {
+    const { request } = event
+    if (request.method === 'GET' && request.destination === 'document') {
         return matchPrecache('/offline')
     } else {
         return Response.error() as any
