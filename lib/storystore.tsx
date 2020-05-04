30834dc8b6363acc297f2059005647758cfbf6ca
Carlos A. Cabrera <cacabrer@adobe.com>
2020-05-04 13:22:54 -0500

    persistent data (test)

--- lib/storystore.tsx
+++ lib/storystore.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, Reducer, useReducer } from 'react'
+import React, { createContext, Reducer, useReducer, useEffect } from 'react'
 import { NextPage } from 'next'
 import { COOKIE, getCookie, setCookie } from '~/lib/cookies'
 import { updateSettingsFromCookie } from '~/lib/updateSettingsFromCookie'
@@ -45,11 +45,44 @@ export const StoryStoreContext = createContext({
     },
 })
 
+const CACHE_NAME = 'storystore'
+const FAKE_ENDPOINT = '/local/storystore'
+
+const setDataInCacheStorage = async (payload: any) => {
+    try {
+        const cache = await caches.open(CACHE_NAME)
+        const responseBody = JSON.stringify(payload)
+        const response = new Response(responseBody)
+        await cache.put(FAKE_ENDPOINT, response)
+
+        console.log('Data saved in Cache Storage! 🎉')
+    } catch (error) {
+        // It's up to you how you resolve the error
+        console.log('🤔 There was an issue saving data in Cache Storage', { error })
+    }
+}
+
+const getDataFromCacheStorage = async () => {
+    try {
+        const cache = await caches.open(CACHE_NAME)
+        const response = await cache.match(FAKE_ENDPOINT)
+
+        if (!response) {
+            return null
+        }
+
+        const responseBody = await response.json()
+        return responseBody
+    } catch (error) {
+        // Gotta catch 'em all
+        console.log('🤔 There was an issue getting data from Cache Storage', { error })
+    }
+}
+
 const reducer: Reducer<ReducerState, ReducerActions> = (state, action) => {
     switch (action.type) {
         case 'setCartId':
             setCookie(COOKIE.cartId, action.payload, 365)
-
             return {
                 ...state,
                 cartId: action.payload,
@@ -57,6 +90,7 @@ const reducer: Reducer<ReducerState, ReducerActions> = (state, action) => {
 
         case 'setSettings':
             setCookie(COOKIE.settings, JSON.stringify(action.payload), 365)
+            setDataInCacheStorage({ ...action.payload })
 
             return {
                 ...state,
@@ -77,6 +111,24 @@ export const withStoryStore = (PageComponent: NextPage<any>) => {
             cartId: getCookie(COOKIE.cartId, cookie) || '',
             settings: updateSettingsFromCookie(initialState.settings, cookie),
         })
+
+        /**
+         * Local Storage and Cookie data is not shared between iOS Safair and "installed PWA",
+         * As a workaround, we sync the data from Cache Storage to a Cookie.
+         *
+         * Note: Chrome and Safari only expose `CacheStorage` to the windowed context over HTTPS.
+         * window.caches will be undefined unless an SSL certificate is configured.
+         * https://medium.com/@kozak.jakub55/how-to-share-state-data-between-a-pwa-in-ios-safari-and-standalone-mode-64174a48b043
+         */
+        useEffect(() => {
+            const dataFromCookie = getCookie(COOKIE.settings)
+            if (!dataFromCookie) {
+                getDataFromCacheStorage().then(payload => {
+                    console.log('🚚 Transfering data from Cache Storage to Cookie')
+                    setCookie(COOKIE.settings, JSON.stringify(payload), 365)
+                })
+            }
+        }, [dispatch])
 
         return (
             <StoryStoreContext.Provider
