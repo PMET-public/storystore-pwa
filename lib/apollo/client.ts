e8996c75c7834ec021fadb03d616afb723c6a802
Carlos A. Cabrera <cacabrer@adobe.com>
2020-05-06 00:44:52 -0500

    testing restoring data

--- lib/apollo/client.ts
+++ lib/apollo/client.ts
@@ -92,7 +92,7 @@ function create(magentoUrl?: string, initialState: any = {}, cookie?: string) {
                     return defaultDataIdFromObject(object)
             }
         },
-    })
+    }).restore(initialState)
 
     if (process.browser) {
         // await before instantiating ApolloClient, else queries might run before the cache is persisted
@@ -102,10 +102,8 @@ function create(magentoUrl?: string, initialState: any = {}, cookie?: string) {
         })
     }
 
-    cache.restore(initialState)
-
     cache.writeData({
-        data: { ...defaults },
+        data: { ...defaults, ...initialState },
     })
 
     const client = new ApolloClient({
