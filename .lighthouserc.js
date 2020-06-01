dbd6f57f26d5c4c95b1191fd39fe6ca375c1a278
Keith Bentrup <keith@keithbentrup.com>
2020-06-01 17:15:32 -0400

    increase timeout

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -4,7 +4,7 @@ module.exports = {
       url: ['http://localhost:3000/'],
       startServerCommand: 'npm start',
       startServerReadyPattern: 'listen|Ready',
-      startServerReadyTimeout: 30000,
+      startServerReadyTimeout: 60000,
     },
     upload: {
       target: 'temporary-public-storage',
