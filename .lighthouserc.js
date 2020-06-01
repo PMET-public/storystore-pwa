e5b2b4daf17c5a7b36b2204cd1ad3161906f0364
Keith Bentrup <keith@keithbentrup.com>
2020-06-01 16:52:29 -0400

    configure startservercommand

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -1,5 +1,11 @@
 module.exports = {
   ci: {
+    collect: {
+      url: ['http://localhost:3000/'],
+      startServerCommand: 'npm start',
+      startServerReadyPattern: 'listen|ready',
+      startServerReadyTimeout: 30000,
+    },
     upload: {
       target: 'temporary-public-storage',
     },
