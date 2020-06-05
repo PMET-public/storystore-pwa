262732342c06faadad47c77eda55eecf85ac7f8f
Carlos A. Cabrera <cacabrer@adobe.com>
2020-06-05 12:51:29 -0500

    integration

--- @types/env.d.ts
+++ @types/env.d.ts
@@ -3,5 +3,8 @@ declare namespace NodeJS {
         MAGENTO_URL: string
         GOOGLE_ANALYTICS: string
         DEMO_MODE: string
+
+        LATEST_RELEASE_REDIRECT_URL: string
+        PREV_RELEASE_REDIRECT_URL: string
     }
 }
--- pages/api/check-endpoint.ts
+++ pages/api/check-endpoint.ts
@@ -1,8 +1,9 @@
-import { engines } from '~/package.json'
+import { magentoDependency } from '~/package.json'
 import { NextApiRequest, NextApiResponse } from 'next'
 import createApolloClient from '~/lib/apollo/client'
 import gql from 'graphql-tag'
 import semver from 'semver'
+import { URL } from 'url'
 
 export const config = {
     api: {
@@ -11,11 +12,11 @@ export const config = {
 }
 
 enum ErrorMessages {
-    'INVALID_URL' = '😑 Invalid URL. Please try again.',
+    'INVALID_URL' = '😑 Please enter the URL to your Magento instance.',
     'NOT_FOUND' = '🤔 Not found. Please check the URL and try again.',
-    'MISSING_STORYSTORE' = `🤔 This Magento Instance seems to be missing the StoryStore Module. Please install and try again.`,
-    'UNAUTHENTICATED' = `🔐 This Magento Instance is private. Don't forget to include credentials. e.g. https://user@password:...".`,
-    'INTERNAL' = '🥴 There is an issue connecting to the Magento Instance. Please try again later.',
+    'MISSING_STORYSTORE' = `🤔 This Magento instance is missing the StoryStore Module.`,
+    'UNAUTHENTICATED' = `🔐 This Magento instance is private. Don't forget to include credentials. e.g. https://user@password:...".`,
+    'INTERNAL' = '🥴 There is an issue connecting to the Magento instance. Please try again later.',
 }
 
 enum ErrorLevels {
@@ -35,16 +36,21 @@ export type ErrorResponse = {
 }
 
 export type Response = {
+    magentoVersion?: string
+    magentoDependency?: string
+    storyStoreModule?: boolean
+    redirectTo?: string
     errors?: ErrorResponse[]
 }
 
 export const CheckEndpointApi = async (req: NextApiRequest, res: NextApiResponse<Response>) => {
+    if (!magentoDependency) throw Error()
+
     const url = req.query.url?.toString()
+    const includeOtherReleases = Number(req.query.includeOtherReleases ?? 1)
 
     if (!url) {
-        res.status(406)
-
-        res.send({
+        return res.status(400).send({
             errors: [
                 {
                     level: ErrorLevels.ERROR,
@@ -53,8 +59,6 @@ export const CheckEndpointApi = async (req: NextApiRequest, res: NextApiResponse
                 },
             ],
         })
-
-        return
     }
 
     try {
@@ -65,33 +69,70 @@ export const CheckEndpointApi = async (req: NextApiRequest, res: NextApiResponse
                 query GetMagentoVersion {
                     storeConfig {
                         id
-                        # version: storystore_magento_version
+                        version: storystore_pwa_magento_version
                     }
                 }
             `,
+            errorPolicy: 'ignore',
         })
 
-        const version = data?.version
+        // Get version of Magento Instance
+        const magentoVersion = semver.coerce(data?.storeConfig.version)?.version ?? '2.3.4'
+        // Check if the StoryStory Module is installed
+        const storyStoreModule = Boolean(data?.storeConfig.version)
 
-        if (!semver.satisfies(version, engines.magento)) {
-            // Redirect to Previous URL if available
-            // TODO Return error message warning the user to upgrade to latest.
-            if (process.env.PREV_URL) {
-                res.writeHead(302, {
-                    Location: process.env.PREV_UR,
+        // Releases Redirect URLS
+        const latestReleaseRedirectUrl = process.env.LATEST_RELEASE_REDIRECT_URL && new URL(process.env.LATEST_RELEASE_REDIRECT_URL)
+        const prevReleaseRedirectUrl = process.env.PREV_RELEASE_REDIRECT_URL && new URL(process.env.PREV_RELEASE_REDIRECT_URL)
+
+        // TODO: Comment
+        if (includeOtherReleases === 1 && latestReleaseRedirectUrl && req.headers.host !== latestReleaseRedirectUrl.host) {
+            const latest = await fetch(new URL(`/api/check-endpoint?url=${url}&includeOtherReleases=0`, latestReleaseRedirectUrl.href).href).then(r => r.json())
+
+            if (!latest.errors) {
+                return res.send({
+                    ...latest,
+                    redirectTo: latestReleaseRedirectUrl.href,
                 })
             }
+        }
 
-            return res.status(406).send({
+        // If the Magento Instance is supported ...
+        if (!semver.satisfies(magentoVersion, magentoDependency)) {
+            res.status(422)
+
+            /** ... lets check if the previous release supports this version of Magento */
+            if (includeOtherReleases === 1 && prevReleaseRedirectUrl && req.headers.host !== prevReleaseRedirectUrl.host) {
+                const prev = await fetch(new URL(`/api/check-endpoint?url=${url}&includeOtherReleases=0`, prevReleaseRedirectUrl).href).then(r => r.json())
+
+                if (!prev.errors) {
+                    return res.send({
+                        ...prev,
+                        redirectTo: prevReleaseRedirectUrl.href,
+                    })
+                }
+            }
+
+            return res.send({
+                magentoVersion,
+                magentoDependency,
+                storyStoreModule,
                 errors: [
                     {
                         level: ErrorLevels.ERROR,
                         key: Fields.magentoUrl,
-                        message: `Sorry, Magento ${version} is not supported by this Storefront. Please make sure to use Magento ${engines.magento}.`,
+                        message: `Magento ${magentoVersion} is not supported by this Storefront. Please use Magento ${magentoDependency}.`,
                     },
                 ],
             })
         }
+
+        // 👌 All good!
+        return res.send({
+            magentoVersion,
+            magentoDependency,
+            storyStoreModule,
+        })
     } catch (error) {
         res.status(error.networkError?.statusCode ?? 500)
 
@@ -121,19 +162,6 @@ export const CheckEndpointApi = async (req: NextApiRequest, res: NextApiResponse
             })
         }
 
-        /** Missing StoryStore */
-        if (error.graphQLErrors) {
-            return res.send({
-                errors: [
-                    {
-                        level: ErrorLevels.ERROR,
-                        key: Fields.magentoUrl,
-                        message: ErrorMessages.MISSING_STORYSTORE,
-                    },
-                ],
-            })
-        }
-
         return res.send({
             errors: [
                 {
@@ -144,8 +172,6 @@ export const CheckEndpointApi = async (req: NextApiRequest, res: NextApiResponse
             ],
         })
     }
-
-    return res.send({ errors: undefined })
 }
 
 export default CheckEndpointApi
