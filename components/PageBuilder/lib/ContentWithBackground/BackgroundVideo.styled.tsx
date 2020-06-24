67c5be31c42cd4351caf915a0de299b268adf723
Carlos A. Cabrera <cacabrer@adobe.com>
2020-06-24 08:53:20 -0500

    style fixes

--- components/PageBuilder/lib/ContentWithBackground/BackgroundVideo.styled.tsx
+++ components/PageBuilder/lib/ContentWithBackground/BackgroundVideo.styled.tsx
@@ -1,16 +1,12 @@
 import styled from 'styled-components'
 
 export const Root = styled.div`
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    left: 0;
-    top: 0;
+    position: relative;
     z-index: 0;
 
     & [id*='jarallax-container'] video,
     & [id*='jarallax-container'] iframe {
-        transition: opacity 300ms ease;
+        transition: opacity 500ms ease;
         opacity: 0;
         z-index: 1 !important;
     }
