59342f173ed69cf5e3d68b5dd17d1b4e4cc834de
Carlos A. Cabrera <cacabrer@adobe.com>
2020-07-08 14:33:35 -0500

    background overlay color

--- components/PageBuilder/lib/ContentWithBackground/BackgroundVideo.styled.tsx
+++ components/PageBuilder/lib/ContentWithBackground/BackgroundVideo.styled.tsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
 
-export const Root = styled.div`
+export const Root = styled.div<{ $overlayColor?: string }>`
     position: absolute;
     width: 100%;
     height: 100%;
@@ -14,4 +14,19 @@ export const Root = styled.div`
         opacity: 0;
         z-index: 1 !important;
     }
+
+    ${props =>
+        props.$overlayColor &&
+        `
+            &::after {
+                content: "";
+                display: block;
+                position: absolute;
+                top: 0;
+                right: 0;
+                left: 0;
+                bottom: 0;
+                background-color: ${props.$overlayColor};
+            }
+        `}
 `
