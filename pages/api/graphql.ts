12a173ec2cd40f5d20b124e4d11aeb5df0fda580
Carlos A. Cabrera <cacabrer@adobe.com>
2020-08-05 10:28:01 -0500

    Proxy Middleware

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,8 +1,8 @@
 import { COOKIE } from '~/lib/cookies'
-import https, { RequestOptions } from 'https'
-import http from 'http'
 import { URL } from 'url'
 import { NextApiRequest, NextApiResponse } from 'next'
+import { createProxyMiddleware } from 'http-proxy-middleware'
+import { runApiMiddleware } from '~/lib/runApiMiddleware'
 
 export const config = {
     api: {
@@ -10,59 +10,23 @@ export const config = {
     },
 }
 
-const proxyGraphQl = async (request: NextApiRequest, response: NextApiResponse) =>
-    new Promise(resolve => {
-        let settings = {
-            magentoUrl: process.env.MAGENTO_URL,
+const proxyGraphQl = async (req: NextApiRequest, res: NextApiResponse) => {
+    let settings = {
+        magentoUrl: process.env.MAGENTO_URL,
+    }
+
+    if (Boolean(process.env.CLOUD_MODE)) {
+        settings = {
+            ...settings,
+            ...JSON.parse(req.cookies[COOKIE.settings] || '{}'),
         }
+    }
 
-        if (Boolean(process.env.CLOUD_MODE)) {
-            settings = {
-                ...settings,
-                ...JSON.parse(request.cookies[COOKIE.settings] || '{}'),
-            }
-        }
+    const query = req.url?.split('?')[1]
 
-        const query = request.url?.split('?')[1]
+    const url = new URL('graphql' + (query ? `?${query}` : ''), settings.magentoUrl)
 
-        const magentoUrl = new URL('graphql' + (query ? `?${query}` : ''), settings.magentoUrl)
-
-        const options: RequestOptions = {
-            method: request.method,
-            headers: {
-                ...request.headers,
-                host: magentoUrl.host,
-            },
-        }
-
-        const httpx = magentoUrl.protocol === 'https:' ? https : http
-
-        const proxy = httpx
-            .request(magentoUrl, options, res => {
-                response.writeHead(res.statusCode as number, res.headers)
-
-                res.pipe(response, {
-                    end: true,
-                })
-            })
-            .on('error', (error: any) => {
-                if (error) {
-                    if (error.code === 'ENOTFOUND') {
-                        response.status(404)
-                    } else {
-                        response.status(500)
-                        console.error(error)
-                    }
-
-                    response.end()
-                }
-            })
-
-        request
-            .pipe(proxy, {
-                end: true,
-            })
-            .on('response', resolve)
-    })
+    await runApiMiddleware(req, res, createProxyMiddleware({ target: url.href, changeOrigin: true, logLevel: 'error' }))
+}
 
 export default proxyGraphQl
