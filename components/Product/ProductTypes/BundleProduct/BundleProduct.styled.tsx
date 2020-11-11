320e2226c266f6ae17fa46837af2d91e9f201954
Carlos A. Cabrera <cacabrer@adobe.com>
2020-11-11 09:40:21 -0600

    wip

--- components/Product/ProductTypes/BundleProduct/BundleProduct.styled.tsx
+++ components/Product/ProductTypes/BundleProduct/BundleProduct.styled.tsx
@@ -3,7 +3,27 @@ import styled from 'styled-components'
 export const Root = styled.div`
     display: grid;
     grid-auto-rows: max-content;
-    grid-gap: 3rem;
+    grid-gap: 4rem;
+`
+
+export const Configuration = styled.div`
+    display: grid;
+    grid-auto-rows: max-content;
+    grid-gap: 2rem;
+`
+
+export const Fieldset = styled.fieldset`
+    display: grid;
+    grid-auto-rows: max-content;
+    grid-gap: 2rem;
+    background-color: ${props => props.theme.colors.onSurface5};
+    border-radius: 2rem;
+    padding: 2rem;
+
+    &:not(:last-of-type) {
+        padding-bottom: 3rem;
+        border-bottom: 0.1rem solid ${props => props.theme.colors.onSurface5};
+    }
 `
 
 export const OptionLabel = styled.span`
@@ -11,6 +31,13 @@ export const OptionLabel = styled.span`
     grid-gap: 1rem;
     /* grid-auto-flow: column; */
     /* grid-auto-columns: max-content; */
-    grid-template-columns: 1fr auto auto;
+    grid-template-columns: 1fr auto;
     align-items: center;
+`
+
+export const PriceWrapper = styled.span`
+    display: grid;
+    grid-auto-flow: column;
+    grid-auto-columns: max-content;
+    font-weight: 600;
 `
--- components/Product/ProductTypes/BundleProduct/BundleProduct.tsx
+++ components/Product/ProductTypes/BundleProduct/BundleProduct.tsx
@@ -1,14 +1,12 @@
 import React, { FunctionComponent, useCallback, useState } from 'react'
 import { BUNDLE_PRODUCT_QUERY } from '.'
-import { Root, OptionLabel } from './BundleProduct.styled'
+import { Root, Configuration, Fieldset, OptionLabel, PriceWrapper } from './BundleProduct.styled'
 import Form, { Input, Error, TextSwatches, Select, Checkbox, Quantity } from '@storystore/ui/dist/components/Form'
 import Button from '@storystore/ui/dist/components/Button'
 import { useCart } from '~/hooks/useCart/useCart'
 import { useStoryStore } from '~/lib/storystore'
 import { useRouter } from 'next/router'
 import { useQuery } from '@apollo/client'
-import Price from '@storystore/ui/dist/components/Price'
-import { priceDataToProps } from '~/components/Product'
 
 export type BundleProductProps = {
     sku: string
@@ -32,11 +30,24 @@ export const BundleProduct: FunctionComponent<BundleProductProps> = ({ sku, urlK
 
     const history = useRouter()
 
+    const [selections, setSelections] = useState([])
+
+    const handleOnValues = useCallback(
+        ({ items }) => {
+            const _selections = items[0].bundle_options.map((x, i) => product.items[i].options.find(y => y.product.id === Number(x.value))?.canChangeQuantity)
+
+            console.log('handleOnValues', { items, product, _selections })
+
+            setSelections(_selections)
+        },
+        [product]
+    )
+
     const handleAddToCart = useCallback(
-        async ({ items, options }) => {
+        async ({ items }) => {
             if (!cartId || !inStock || addingSimpleProductsToCart.loading) return
 
-            console.log('handleAddToCart', { items, options })
+            console.log('handleAddToCart', { items })
             return
 
             try {
@@ -57,42 +68,60 @@ export const BundleProduct: FunctionComponent<BundleProductProps> = ({ sku, urlK
     if (loading && !product) return <>⏲ Loading...</>
 
     return (
-        <Root as={Form} onSubmit={handleAddToCart}>
+        <Root as={Form} onSubmit={handleAddToCart} onValues={handleOnValues}>
             <Input name="items[0].data.sku" type="hidden" value={sku} rules={{ required: true }} />
+            <Input name="items[0].data.quantity" type="hidden" value={1} rules={{ required: true }} />
 
-            {product.items
-                ?.map(({ id, label, required, type, options }: any) => ({
-                    _id: id,
-                    type,
-                    swatches: {
-                        label,
-                        name: `options[]`,
-                        rules: { required },
-                        items: options
-                            .map(({ id: optionId, label, quantity, product }: any) => {
-                                return {
-                                    _id: optionId,
-                                    label: (
-                                        <OptionLabel>
-                                            {label} <Price {...priceDataToProps(product.price)} /> x {quantity}
-                                        </OptionLabel>
-                                    ),
-                                    value: product.id,
-                                }
-                            })
-                            .sort((a: any, b: any) => b.position - a.position),
-                    },
-                }))
-                .sort((a: any, b: any) => b.position - a.position)
-                .map(({ _id, type, swatches }: any, index: number) => {
-                    return (
-                        <fieldset key={_id || index}>
-                            {/* {type === 'TextSwatchData' && <TextSwatches hideError {...swatches} />} */}
-                            {/* {type === undefined && <Select hideError blankDefault={`Select an option...`} {...swatches} />} */}
-                            {type === 'checkbox' && <Checkbox {...swatches} />}
-                        </fieldset>
-                    )
-                })}
+            <Configuration>
+                {product.items
+                    ?.map(({ id, label, required, type, options }: any, index: number) => ({
+                        _id: id,
+                        type,
+                        swatches: {
+                            label,
+                            name: `items[0].bundle_options[${index}].value`,
+                            rules: { required },
+                            items: options
+                                .map(({ id: optionId, label, isDefault, product }: any) => {
+                                    return {
+                                        _id: optionId,
+                                        checked: isDefault,
+                                        label:
+                                            type === 'select' ? (
+                                                `${label} +${product.price.minimum.regular.value.toLocaleString('en-US', { style: 'currency', currency: product.price.minimum.regular.currency })}`
+                                            ) : (
+                                                <OptionLabel>
+                                                    {label} [{optionId}]
+                                                    <PriceWrapper>
+                                                        +{product.price.minimum.regular.value.toLocaleString('en-US', { style: 'currency', currency: product.price.minimum.regular.currency })}
+                                                    </PriceWrapper>
+                                                </OptionLabel>
+                                            ),
+                                        value: product.id,
+                                    }
+                                })
+                                .sort((a: any, b: any) => b.position - a.position),
+                        },
+                    }))
+                    .sort((a: any, b: any) => b.position - a.position)
+                    .map(({ _id, type, swatches }: any, index: number) => {
+                        return (
+                            <Fieldset key={_id || index}>
+                                {type === 'radio' && <Checkbox hideError type="radio" {...swatches} />}
+
+                                {type === 'select' && <Select hideError blankDefault={`Select an option...`} {...swatches} />}
+
+                                {type === 'multi' && <TextSwatches hideError type="checkbox" {...swatches} />}
+
+                                {type === 'checkbox' && <Checkbox hideError {...swatches} />}
+
+                                <Quantity name={`items[0].bundle_options[${index}].quantity`} hideError rules={{ required: true }} disabled={!selections[index]} />
+
+                                <Input name={`items[0].bundle_options[${index}].id`} type="hidden" value={_id} rules={{ required: true }} />
+                            </Fieldset>
+                        )
+                    })}
+            </Configuration>
 
             <Button type="submit" as="button" text={inStock ? 'Add to Cart' : 'Sold Out'} disabled={!inStock} loading={false} />
 
